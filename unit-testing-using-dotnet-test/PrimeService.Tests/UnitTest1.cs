0e6717a4d3f7cebe90c44275969a9d5b604dfc37
Khanh Tran <khanh.tranxuan@euroland.com>
2024-03-14 11:29:26 +0700

    add code

---
+++ unit-testing-using-dotnet-test/PrimeService.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Xunit;
+namespace PrimeService.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void IsPrime_InputIs1_ReturnFalse()
+    {
+        var primeService = new PrimeService();
+
+        bool result = primeService.IsPrime(1);
+
+        Assert.False(result, "1 should not be prime");
+    }
+}
---
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PrimeService;
+
+public class PrimeService
+{
+    public bool IsPrime(int candidate)
+    {
+        throw new NotImplementedException("Not implemented");
+    }
+}
